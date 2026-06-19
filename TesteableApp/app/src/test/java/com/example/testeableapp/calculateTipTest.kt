be89bc150459979b2b18b13b8a716ce454cebd4d
CarlosCruz99 <00013324@uca.edu.sv>
2026-06-18 22:38:14 -0600

    docs: Agregar comentarios a pruebas unitarias

--- TesteableApp/app/src/test/java/com/example/testeableapp/calculateTipTest.kt
+++ TesteableApp/app/src/test/java/com/example/testeableapp/calculateTipTest.kt
@@ -5,24 +5,52 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class calculateTipTest {
+
+    /**
+     * Test: Cálculo de la propina con un porcentaje de 20% muestra el resultado correcto.
+     *
+     * Verifica que la función "calculateTip" retorna el resultado
+     * esperado cuando tanto el "amount" como el "tipPercent" son positivos.
+     */
     @Test
     fun calculationWith20PercentTipShowsCorrectResult(){
         val result = calculateTip(80.0, 20, false)
         assertEquals(16.0,result)
     }
 
+    /**
+     * Test: Cálculo de la propina con un porcentaje de 15%
+     * y redondeo muestra el resultado correcto.
+     *
+     * Verifica que la función "calculateTip" retorna el resultado
+     * esperado cuando tanto el "amount" como el "tipPercent" son positivos
+     * y se aplica el redondeo.
+     */
     @Test
     fun calculationWith15PercentTipAndRoundUpShowsCorrectResult(){
         val result = calculateTip(85.0, 15, true)
         assertEquals(13.0,result)
     }
 
+    /**
+     * Test: Cálculo de la propina con un "amount" negativo debe dar 0.
+     *
+     * Verifica el funcionamiento de la función "calculateTip" con un
+     * "amount" negativo, aunque esta tendría que retornar un resultado
+     * de 0, actualmente solo realiza la operación con el valor negativo
+     */
     @Test
     fun calculationWithNegativeInputShows0(){
         val result = calculateTip(-85.0, 15, true)
         assertEquals(0.0,result)
     }
 
+    /**
+     * Test: Cálculo del total a pagar por persona, incluyendo propina
+     *
+     * Verifica que la lógica utilizada para obtener el monto total que
+     * debe de pagar cada persona retorna un resultado correcto.
+     */
     @Test
     fun calculationForTotalShowsCorrectResult(){
         val bill = 80.0
